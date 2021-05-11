a21382a053a1ca3973bea84c26493f263a0a21d8
João Emerson <joaoniculau@gmail.com>
2021-05-11 11:05:59 -0300

    INITIAL COMMIT

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/hello')
+@app.route('/')
+def hello():
+    return 'Hello, World'
+
+
+@app.route('/identifyinsect', methods=['POST'])
+def upload_file():
+    f = request.files['the_file']
+    f.save('./uploads/%s' % f.filename)
+
+    #codigo aqui!
+    #O retorno pode ser um JSON
+    return "OK"
+
+if __name__ == "__main__":
+    app.run(debug=True)
